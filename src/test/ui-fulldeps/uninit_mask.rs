eab123763525b68dd99d19f9d6e751ccf7533022
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-04-23 18:15:37 -0400

    Mispelled.

--- src/test/ui-fulldeps/uninit_mask.rs
+++ src/test/ui-fulldeps/uninit_mask.rs
@@ -11,7 +11,7 @@ use rustc_middle::mir::interpret::UninitMask;
 use rustc_target::abi::Size;
 
 fn main() {
-    let mut mask = Uninit::new(Size::from_bytes(500), false);
+    let mut mask = UninitMask::new(Size::from_bytes(500), false);
     assert!(!mask.get(Size::from_bytes(499)));
     mask.set(Size::from_bytes(499), true);
     assert!(mask.get(Size::from_bytes(499)));
